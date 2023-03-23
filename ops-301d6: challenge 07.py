b4045f3589aadf15bb0975cc5515130bd1944029
R00sterGuy <alexander88echols@gmail.com>
2023-03-23 00:02:43 +0000

    Created a python list and manipulated it

---
+++ ops-301d6: challenge 07.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+#Script: OPS 301 Class 08 Ops Challenge: Python list manipulation
+#Author: Alexander Echols
+# Date of Creation: 22 Mar 2023
+# Date of latest revision: 22 Mar 2023
+# Purpose: Create a python script that manipulates a list
+
+# Main
+# Create a list
+AlsList = ['Webcam', 'TV', 'Tea', 'Water', 'Pin', 'Mic', 'Notepad', 'Ringlight', 'Nootropics', 'Pizza']
+# Print the fourth element of the list
+print(AlsList[3])
+print(AlsList[5:11])
+AlsList[6] = "onion"
+print(AlsList[6])
+# End
