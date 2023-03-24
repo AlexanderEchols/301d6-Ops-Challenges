1e0afe0f30602a70e4a194b5f078cbfda4b01d90
R00sterGuy <alexander88echols@gmail.com>
2023-03-24 19:19:49 +0000

    commented out the game portion so I can get full credit for the assignment

--- ops-301d6: challenge 08.py
+++ ops-301d6: challenge 08.py
@@ -7,72 +7,76 @@
 # Edit: the requirments are below and commented out. I am trying to create a guessing 
 # game using the parameters of the ops challange
 
-# Main
-# Set age for alex
-Alex = 34
+# # Main
 
-# Guessing game intro
-print("Hello there, I'm Alex. Want to play a game?")
+# Define variables
+a = int(input("what would you like for a?"))
+b = int(input("what would you like for b?"))
 
-# we are going to create a while true loop to play the game
-while True:
-    # Lets start by asking for their guess and storing it in a variable called Guess
-    Guess = int(input("How old do you think I am?"))
-    # create an if statement that uses Equals
-    if Guess == Alex:
-        print("Wow, you got it!!! Are you a Carny?")
-        # we use a "break" statement to leave the loop.
-        break
-    # create an if statemet that uses Not Equals:
-    if Guess != Alex:
-        print("Bad luck, try again")
-    # create an if statement that uses Less than:
-    if Guess < Alex:
-        print("Too low, Thank you though")
-    # create an if statement that uses Less than or equal to:
-    if Guess <= Alex:
-        print("yes and no, Try again but try higher")
+# Equals
+if a == b:
+    print("a is equal to b")
+    
+# Not Equals
+if a != b:
+    print("a is not equal to b")
+    
+# Less than
+if a < b:
+    print("a is less than b")
+    
+# Less than or equal to
+if a <= b:
+    print("a is less than or equal to b")
+    
+# Greater than
+if a > b:
+    print("a is greater than b")
+    
+# Greater than or equal to
+if a >= b:
+    print("a is greater than or equal to b")
+    
+# If statement using logical conditional of your choice with elif
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is equal to b")
+    
+# If statement with both elif and else
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is equal to b")
+#end
 
-# # Define variables
-# a = 55
-# b = 86
+# Secondary main. This is where I am trying to make the game work.
+# # Set age for alex
+# Alex = 34
 
-# # Equals
-# if a == b:
-#     print("a is equal to b")
-    
-# # Not Equals
-# if a != b:
-#     print("a is not equal to b")
-    
-# # Less than
-# if a < b:
-#     print("a is less than b")
-    
-# # Less than or equal to
-# if a <= b:
-#     print("a is less than or equal to b")
-    
-# # Greater than
-# if a > b:
-#     print("a is greater than b")
-    
-# # Greater than or equal to
-# if a >= b:
-#     print("a is greater than or equal to b")
-    
-# # If statement using logical conditional of your choice with elif
-# if a < b:
-#     print("a is less than b")
-# elif a > b:
-#     print("a is greater than b")
-# else:
-#     print("a is equal to b")
-    
-# # If statement with both elif and else
-# if a < b:
-#     print("a is less than b")
-# elif a > b:
-#     print("a is greater than b")
-# else:
-#     print("a is equal to b")
+# # Guessing game intro
+# print("Hello there, I'm Alex. Want to play a game?")
+
+# # we are going to create a while true loop to play the game
+# while True:
+#     # Lets start by asking for their guess and storing it in a variable called Guess
+#     Guess = int(input("How old do you think I am?"))
+#     # create an if statement that uses Equals
+#     if Guess == Alex:
+#         print("Wow, you got it!!! Are you a Carny?")
+#         # we use a "break" statement to leave the loop.
+#         break
+#     # create an if statemet that uses Not Equals:
+#     if Guess != Alex:
+#         print("Bad luck, try again")
+#     # create an if statement that uses Less than:
+#     if Guess < Alex:
+#         print("Too low, Thank you though")
+#     # create an if statement that uses Less than or equal to:
+#     if Guess <= Alex:
+#         print("yes and no, Try again but try higher")
+
