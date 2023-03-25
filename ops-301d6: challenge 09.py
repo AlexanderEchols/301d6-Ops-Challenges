2f40c1afe14f83f5d8576423fe3dfa8bc49ef1d9
R00sterGuy <alexander88echols@gmail.com>
2023-03-25 19:38:52 +0000

    edited it to be more complete

--- ops-301d6: challenge 09.py
+++ ops-301d6: challenge 09.py
@@ -1,23 +1,36 @@
-# Open a new file in write mode
-file = open("example.txt", "w")
+#!/bin/python3
+# Script: Ops 301 Class 09 Ops Challenges: Python File Handling
+# Author: Alexander Echols.    
+# Date of creation: 24 Mar 2023                
+# Date of latest revision: 25 Mar 2023.      
+# Purpose: Write a Python script that uses logical statements to create a guessing game.
 
-# Append three lines to the file
-file.write("This is the first line\n")
-file.write("This is the second line\n")
-file.write("This is the third line\n")
+# # Main
+
+# we will create a new file in write mode 
+Newfile = open("WhoDis.txt", "w")
+
+# Then we need to append three lines to the file
+# First
+Newfile.write("One is the lonelest number that you'll ever do'\n")
+# Second
+Newfile.write("Two can be as bad as one, it's the loneliest number sinch the number one\n")
+# Third
+Newfile.write("And three is just a crowd. There was no line in the song with three\n")
+
+# Now we need to close the file
+Newfile.close()
+
+# Re-open it in read mode
+Newfile = open("WhoDis.txt", "r")
+
+# Read the first line of the file, then print it to the screen
+print(Newfile.readline())
 
 # Close the file
-file.close()
-
-# Open the file in read mode
-file = open("example.txt", "r")
-
-# Read the first line of the file and print it to the screen
-print(file.readline())
-
-# Close the file
-file.close()
+Newfile.close()
 
 # Delete the file
 import os
-os.remove("example.txt")
+os.remove("WhoDis.txt")
+# end
